04dcd01f2036569f7242f073cce1557c54f704a0
minhazf456 <minhazf@gmail.com>
2020-06-18 23:55:20 -0700

    solved InflightEntertainMent problems with efficient solution

---
+++ HashMaps/InflightEntertainMent.java
@@ -0,0 +1,34 @@
+package HashMaps;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InflightEntertainMent {
+
+    public static boolean canTwoMovieFillFlight (int [] movieLengths, int flighLength) {
+        // lets create hashset first
+        Set<Integer> movieLenghtsSeen = new HashSet<>();
+
+        for (int firstMovieLength : movieLengths) {
+            int matchingSecondMovieLenght = flighLength - firstMovieLength;
+            if (movieLenghtsSeen.contains(matchingSecondMovieLenght)) {
+                return true;
+            } else {
+                movieLenghtsSeen.add(firstMovieLength);
+            }
+
+        }
+        return false;
+
+    }
+
+
+
+    public static void main (String[] args){
+      int [] movieLengths = {1,4,5,7};
+      canTwoMovieFillFlight(movieLengths, 6);
+      final boolean flightLengthswhat = canTwoMovieFillFlight(movieLengths, 6);
+        System.out.println(flightLengthswhat);
+
+    }
+}
