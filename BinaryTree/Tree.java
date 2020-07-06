247ad887904c4e726d99a7efba5ea3f27668ee13
minhazf456 <minhazf@gmail.com>
2020-07-06 00:31:18 -0700

    Binary TreeInsertion added

---
+++ BinaryTree/Tree.java
@@ -0,0 +1,36 @@
+package BinaryTree;
+
+// now we need class for tree itself.
+// we only need one field in the tree
+//and that's the root node because if we have the root we
+// can get to everything else.
+
+
+
+public class Tree {
+//    we just need one thing is root
+    //if we have the root node we can get to everything else
+    // by traversing it's left child and it's right child.
+    private TreeNode root;
+
+//We need a class for the tree and a class for the nodes that will go into the tree.
+//Alright so to build a binary tree we're going to need to insert nodes and
+//the way we're gonna do it, and we went through this on the slides is,
+//if we come to insert a node, if the tree is empty then the node
+//becomes the root and we're done.if the tree isn't empty we're gonna
+//compare the value we're inserting against the root node.
+
+// If it's less than the root node we're going to go to the left and if it's greater
+//than the root node we're going to go to the right and then we're gonna rinse and repeat.
+
+    public void insert(int value) {
+    if (root == null) {
+        root = new TreeNode(value);
+    }
+    else {
+        root.insert(value);
+    }
+}
+
+
+}
---
+++ BinaryTree/TreeNode.java
@@ -0,0 +1,68 @@
+package BinaryTree;
+
+public class TreeNode {
+    // nodes have 3 fields
+    private int data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+//    adding constructor
+
+    public TreeNode (int data){
+        this.data = data;
+    }
+    // we will not allow duplicate value
+    public void insert(int value) {
+        if (value == data) {
+            return;
+        }
+
+        if (value < data) {
+            if (leftChild == null) {
+                leftChild = new TreeNode(value);
+            }
+            else {
+                leftChild.insert(value);
+            }
+        }
+        else {
+            if (rightChild == null) {
+                rightChild = new TreeNode(value);
+            }
+            else {
+                rightChild.insert(value);
+            }
+        }
+    }
+//
+    public void traverseInOrder(){
+
+    }
+
+
+//    getters and setters
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
