9c26dd25fb5afebfe4c12dad964df7718abc5037
minhazf456 <minhazf@gmail.com>
2020-07-27 00:29:01 -0700

    Insertion at the head in a SinglyLinkedList

--- LinkedList/ListDemo.java
+++ LinkedList/ListDemo.java
@@ -3,6 +3,10 @@ package LinkedList;
 public class ListDemo {
     public static void main( String args[] ) {
         SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
-        System.out.println(sll.isEmpty());
+        sll.printList();
+        for (int i = 1; i <= 10; i++) {
+            sll.insertAtHead(i);
+            sll.printList();
+        }
     }
 }
--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -21,5 +21,30 @@ public class SinglyLinkedList <T>{
         return false;
     }
 
+    /// inserts new data at the end of the linked list
 
+    public void insertAtHead ( T data) {
+        /// creating a new node and assigning it the new data value
+        Node newNode = new Node();
+        newNode.data = data;
+        // linking head to the newNode's nextNode
+        newNode.nextNode = headNode;
+        headNode = newNode;
+        size++;
+    }
+    // Helper Function to printList
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is Empty!");
+            return;
+        }
+        Node temp = headNode;
+        System.out.print("List : ");
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " -> ");
+            temp = temp.nextNode;
+        }
+        System.out.println(temp.data.toString() + " -> null");
+    }
 }
+
